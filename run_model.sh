adc2b5a4327e5b35114beadd585389425fa37153
m4theus4ndr4de <matheus.andrade.lp@gmail.com>
2022-11-28 21:11:21 -0300

    finish aws notebook

--- run_model.sh
+++ run_model.sh
@@ -1,1 +1,1 @@
-papermill notebooks/clustering_c9_deploy.ipynb notebooks/reports/clustering_c9_deploy_$(date "+%Y-%m-%d_%H:%M:%S").ipynb
+papermill notebooks/clustering_c9_deploy_aws.ipynb notebooks/reports/clustering_c9_deploy_aws$(date "+%Y-%m-%d_%H:%M:%S").ipynb
